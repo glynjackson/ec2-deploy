27efe78f12ab58ca397631eceed0892fc4ec90aa
GlynJackson <me@glynjackson.org>
2015-03-31 14:00:06 +0100

    added function to number of workers based available virtual or physical CPUs on this system

--- server_templates/AWS_Ubuntu14_04_LTS/gunicorn.conf.py
+++ server_templates/AWS_Ubuntu14_04_LTS/gunicorn.conf.py
@@ -1,10 +1,32 @@
 import os
 import dotenv
-# Load environment vars.
+
+
+def get_workers_count_based_on_cpu_count():
+    """
+    Returns the number of workers based available virtual or physical CPUs on this system.
+    """
+    # Python 2.6+
+    try:
+        import multiprocessing
+
+        return multiprocessing.cpu_count() * 2 + 1
+    except (ImportError, NotImplementedError):
+        pass
+    try:
+        res = int(os.sysconf('SC_NPROCESSORS_ONLN'))
+        if res > 0:
+            return res * 2 + 1
+    except (AttributeError, ValueError):
+        pass
+
+    raise Exception('Can not determine number of CPUs on this system')
+
+
 dotenv.load_dotenv('.env')
 
 bind = "127.0.0.1:8002"
-workers = 3
+workers = get_workers_count_based_on_cpu_count()
 worker_class = 'gevent'
 
 max_requests = 1000
